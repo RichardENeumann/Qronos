e432bde0b180416010944cc42b44aed46f8fae08
RichardENeumann <RichardNeumann@googlemail.com>
2023-10-30 17:53:33 +0100

    Add more components

--- Qronos.java
+++ Qronos.java
@@ -1,17 +1,23 @@
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionListener;
  
 public class Qronos {
     public static void main(String[] args) {
-        JFrame mainFrame = new JFrame("Qronos Timekeeper");
+        JFrame mainFrame = new JFrame("Qronos 0.1");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setBounds(200, 250, 640, 480);
-        
         mainFrame.setVisible(true);
+
+        Font heading = new Font("Tahoma", Font.PLAIN, 12);
+        Font normalText = new Font("Tahoma", Font.PLAIN, 16);
 
         JPanel mainContent = new JPanel();
         mainFrame.setContentPane(mainContent);
@@ -19,18 +25,24 @@ public class Qronos {
         mainContent.setLayout(null);
 
         JLabel lbApplicationTitle = new JLabel("A work-time logging application for multiple Projects");  
+        lbApplicationTitle.setBounds(150, 25, 450, 25);
+        lbApplicationTitle.setFont(heading);
         mainContent.add(lbApplicationTitle);
-        lbApplicationTitle.setBounds(128, 24, 400, 24);
         
         JLabel lbSelectProjects = new JLabel("Select Projects");
-        mainContent.add(lbSelectProjects);
         lbSelectProjects.setBounds(100, 64, 128, 18);
+        lbSelectProjects.setFont(normalText);
+        mainContent.add(lbSelectProjects);
         
         JLabel lbActiveProjects = new JLabel("Active Projects");
-        mainContent.add(lbActiveProjects);
         lbActiveProjects.setBounds(420, 64, 128, 18);
+        lbActiveProjects.setFont(normalText);
+        mainContent.add(lbActiveProjects); 
         
-        
-        
+        JButton pauseButton = new JButton("Pause");
+        pauseButton.setFont(normalText);
+        pauseButton.setBounds(280, 350, 128, 24);
+        pauseButton.setFont(heading);
+        mainContent.add(pauseButton);
     }
 }
