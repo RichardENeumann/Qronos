5e4901030c1ddb3bc13a7032b221ca84ecab1228
RichardENeumann <RichardNeumann@googlemail.com>
2023-11-03 10:17:36 +0100

    Move Swing code into constructor method

--- Qronos.java
+++ Qronos.java
@@ -1,48 +1,61 @@
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
+import javax.swing.*;
+import java.awt.Container;
+import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
  
-public class Qronos {
-    public static void main(String[] args) {
-        JFrame mainFrame = new JFrame("Qronos 0.1");
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setBounds(200, 250, 640, 480);
-        mainFrame.setVisible(true);
+public class Qronos extends JFrame {
 
-        Font heading = new Font("Tahoma", Font.PLAIN, 12);
-        Font normalText = new Font("Tahoma", Font.PLAIN, 16);
+    public Qronos() {
+        setTitle("Qronos 0.1");
+        setSize(640, 480);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        JPanel mainContent = new JPanel();
-        mainFrame.setContentPane(mainContent);
-        mainContent.setBorder(new EmptyBorder(5, 5, 5, 5));
-        mainContent.setLayout(null);
-
-        JLabel lbApplicationTitle = new JLabel("A work-time logging application for multiple Projects");  
-        lbApplicationTitle.setBounds(150, 25, 450, 25);
-        lbApplicationTitle.setFont(heading);
-        mainContent.add(lbApplicationTitle);
+        Container mainContent = getContentPane();
+        GroupLayout gl = new GroupLayout(mainContent);
+        mainContent.setLayout(gl);
+                
+        Font heading = new Font("Tahoma", Font.PLAIN, 16);
+        Font normalText = new Font("Tahoma", Font.PLAIN, 12);
         
+        JLabel lbApplicationTitle = new JLabel("A work-time logging application for multiple Projects");  
+        lbApplicationTitle.setFont(normalText);
+        lbApplicationTitle.setBounds(150, 25, 450, 25);
+        mainContent.add(lbApplicationTitle);
+                
         JLabel lbSelectProjects = new JLabel("Select Projects");
         lbSelectProjects.setBounds(100, 64, 128, 18);
-        lbSelectProjects.setFont(normalText);
+        lbSelectProjects.setFont(heading);
         mainContent.add(lbSelectProjects);
-        
+                
         JLabel lbActiveProjects = new JLabel("Active Projects");
         lbActiveProjects.setBounds(420, 64, 128, 18);
-        lbActiveProjects.setFont(normalText);
+        lbActiveProjects.setFont(heading);
         mainContent.add(lbActiveProjects); 
-        
+                
         JButton pauseButton = new JButton("Pause");
         pauseButton.setFont(normalText);
         pauseButton.setBounds(280, 350, 128, 24);
-        pauseButton.setFont(heading);
+        pauseButton.setFont(normalText);
         mainContent.add(pauseButton);
+        pauseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+    }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Qronos mainFrame = new Qronos();
+                mainFrame.setVisible(true);
+            }
+       });
     }
 }
